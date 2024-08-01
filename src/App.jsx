7553f569a727e99e1f2c385c3c053367d305602f
Farahksyu <ksyu.farah@gmail.com>
2024-08-01 23:02:17 +0700

    updated App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 import "./App.css";
-// import Button from "./components/Button/Button";
+import Button from "./components/Button/Button";
 import Header from "./components/Header/Header";
+import Hero from "./components/Hero/Hero";
+import Section from "./components/Section/Section";
 
 function App() {
   return (
     <>
       <Header />
+      <Hero />
+      <Button />
+      <Section />
     </>
   );
 }
