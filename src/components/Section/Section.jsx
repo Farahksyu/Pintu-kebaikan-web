31957cedae64d584ddb78dc1aaa44058f8f5cc71
Farahksyu <ksyu.farah@gmail.com>
2024-08-02 15:06:05 +0700

    updated Section.jsx

--- src/components/Section/Section.jsx
+++ src/components/Section/Section.jsx
@@ -2,31 +2,39 @@ import Button from "../Button/Button";
 function Section() {
   return (
     <>
-      <div className="">
-        <div className="">
+      <div className="flex items-center justify-between text-text p-10 mt-10">
+        <div className="h-full ml-20">
           <img src="src/assets/images/Image.png" alt="" />
         </div>
-        <div className="">
-          <h1> Bantu Anak Yatim sampai Mandiri</h1>
-          <p>
+        <div className="text-text mt-10 mr-10">
+          <h1 className="font-semibold text-2xl">
+            Bantu Anak Yatim sampai Mandiri
+          </h1>
+          <p className="mt-6 text-xl">
             Bantu anak yatim sampai mandiri, dengan data tervalidasi dan laporan
             berkala
           </p>
-          <div>
-            <div className="flex">
+          <div className="mt-6">
+            <div className="flex border border-dotted rounded-lg">
               <img src="src/assets/icons/Sparkle.svg" alt="" />
-              <button>Mustahiq dengan 34 parameter survey</button>
+              <button className="text-lg">
+                Mustahiq dengan 34 parameter survey
+              </button>
             </div>
-            <div className="flex">
+            <div className="flex border border-dotted rounded-lg mt-6 mb-6">
               <img src="src/assets/icons/Sparkle.svg" alt="" />
-              <button>Profil lengkap anak yatim</button>
+              <button className="text-lg">Profil lengkap anak yatim</button>
             </div>
-            <div className="flex">
+            <div className="flex border border-dotted rounded-lg">
               <img src="src/assets/icons/Sparkle.svg" alt="" />
-              <button>Kemudahan lacak aktivitas report dan tracking</button>
+              <button className="text-lg">
+                Kemudahan lacak aktivitas report dan tracking
+              </button>
             </div>
           </div>
-          <Button />
+          <div className="mt-6">
+            <Button />
+          </div>
         </div>
       </div>
     </>
