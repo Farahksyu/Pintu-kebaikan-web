376456caeb415a2244d7a2e4d76f617bf1e3187e
Farahksyu <ksyu.farah@gmail.com>
2024-08-03 21:10:25 +0700

    updated Hero.jsx

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -2,43 +2,53 @@ import Button from "../Button/Button";
 
 function Hero() {
   return (
-    <div className="flex -left-1.5">
-      <div className="overflow-x-hidden text-light">
-        <div className="bg-[url('src/assets/images/Hero.png')] absolute w-[761px] h-[630px] z-10 py-12 px-8">
-          <h1 className="text-4xl p-10 font-semibold">
-            Bantu Seluruh Anak Yatim Membangun Masa Depan
-          </h1>
-          <p className="ml-10 mr-10 text-xl">
-            Lorem Ipsum is simply dummy text of the printing and typesetting
-            industry. Lorem Ipsum has been the
-          </p>
-          <div className="flex justify-around text-light -ml-5 mt-10">
-            <div className="">
-              <h1 className="font-bold text-2xl">100+</h1>
-              <p className="text-lg">Lorem ipsum.</p>
-            </div>
-            <div className="">
-              <h1 className="font-bold text-2xl">100+</h1>
-              <p className="text-lg">Lorem ipsum.</p>
-            </div>
-            <div className="">
-              <h1 className="font-bold text-2xl">100+</h1>
-              <p className="text-lg">Lorem ipsum.</p>
+    <>
+      <div className="relative w-screen h-screen overflow-hidden">
+        <div className="flex">
+          <div className="relative w-1/2">
+            <img
+              src="src/assets/images/Hero.png"
+              className="w-full h-screen object-cover"
+              alt="Left Image"
+            />
+            <div className="absolute top-0 left-0 w-full h-full items-center justify-center text-light">
+              <h1 className="text-4xl p-10 font-semibold">
+                Bantu Seluruh <span className="text-secondary">Anak Yatim</span>{" "}
+                Membangun Masa Depan
+              </h1>
+              <p className="ml-10 mr-10 text-xl">
+                Lorem Ipsum is simply dummy text of the printing and typesetting
+                industry. Lorem Ipsum has been the
+              </p>
+              <div className="flex justify-around -ml-5 mt-10">
+                <div className="">
+                  <h1 className="font-bold text-2xl">100+</h1>
+                  <p className="text-lg">Lorem ipsum.</p>
+                </div>
+                <div className="">
+                  <h1 className="font-bold text-2xl">100+</h1>
+                  <p className="text-lg">Lorem ipsum.</p>
+                </div>
+                <div className="">
+                  <h1 className="font-bold text-2xl">100+</h1>
+                  <p className="text-lg">Lorem ipsum.</p>
+                </div>
+              </div>
+              <div className="ml-10 mt-10">
+                <Button />
+              </div>
             </div>
           </div>
-          <div className="ml-10 mt-10">
-            <Button />
+          <div className="relative w-1/2">
+            <img
+              src="src/assets/images/Image.png"
+              className="w-full h-screen object-cover"
+              alt="Right Image"
+            />
           </div>
         </div>
       </div>
-      <div className="overflow-x-hidden">
-        <img
-          className="relative w-[958px] h-[630px] left-[480px] mr-20"
-          src="src/assets/images/Image.png"
-          alt=""
-        />
-      </div>
-    </div>
+    </>
   );
 }
 
