3f92617e38de30ed348c559e589abd80195bd33a
Farahksyu <ksyu.farah@gmail.com>
2024-08-03 21:10:42 +0700

    updated Section.jsx

--- src/components/Section/Section.jsx
+++ src/components/Section/Section.jsx
@@ -2,15 +2,25 @@ import Button from "../Button/Button";
 function Section() {
   return (
     <>
-      <div className="flex items-center justify-between text-text p-10 mt-10">
-        <div className="h-full ml-20">
-          <img src="src/assets/images/Image.png" alt="" />
+      <div className="relative flex items-center justify-between text-text">
+        <div className="relative h-full w-full ml-20">
+          <img
+            className="absolute h-96 -ml-28 -mt-32"
+            src="src/assets/images/bg-ellipse.svg"
+            alt=""
+          />
+          <img
+            className="relative h-60"
+            src="src/assets/images/Image.png"
+            alt=""
+          />
         </div>
-        <div className="text-text mt-10 mr-10">
-          <h1 className="font-semibold text-2xl">
-            Bantu Anak Yatim sampai Mandiri
+        <div className="text-text mr-28">
+          <h1 className="font-bold text-2xl">
+            Bantu <span className="text-primary">Anak Yatim</span> sampai
+            Mandiri
           </h1>
-          <p className="mt-6 text-xl">
+          <p className="mt-6 text-xl text-text">
             Bantu anak yatim sampai mandiri, dengan data tervalidasi dan laporan
             berkala
           </p>
