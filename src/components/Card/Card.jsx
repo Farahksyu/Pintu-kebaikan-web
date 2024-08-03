6ca04a783d5e058a7a41c1a868fe76fdf81315ce
Farahksyu <ksyu.farah@gmail.com>
2024-08-03 21:10:08 +0700

    updated Card.jsx

---
+++ src/components/Card/Card.jsx
@@ -0,0 +1,33 @@
+import Button from "../Button/Button";
+
+function Card() {
+  return (
+    <div className="flex gap-5 mt-10">
+      <div className="text-center mb-10 border bg-light rounded-lg border-[#DDDDDD]">
+        <img className="" src="src/assets/icons/Vectoricon.png" alt="" />
+        <h1>Infaq</h1>
+        <p>Lorem ipsum dolor sit amet consectetur.</p>
+        <Button />
+      </div>
+      <div className="text-center mb-10 border bg-light rounded-lg border-[#DDDDDD]">
+        <img className="" src="src/assets/icons/Vectoricon.png" alt="" />
+        <h1>Infaq</h1>
+        <p>Lorem ipsum dolor sit amet consectetur.</p>
+        <Button />
+      </div>
+      <div className="text-center mb-10 border bg-light rounded-lg border-[#DDDDDD]">
+        <img className="" src="src/assets/icons/Vectoricon.png" alt="" />
+        <h1>Infaq</h1>
+        <p>Lorem ipsum dolor sit amet consectetur.</p>
+        <Button />
+      </div>
+      <div className="text-center mb-10 border bg-light rounded-lg border-[#DDDDDD]">
+        <img className="" src="src/assets/icons/Vectoricon.png" alt="" />
+        <h1>Infaq</h1>
+        <p>Lorem ipsum dolor sit amet consectetur.</p>
+        <Button />
+      </div>
+    </div>
+  );
+}
+export default Card;
