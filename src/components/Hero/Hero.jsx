483977199e0ffb6de05287e93c862d19245d4b9a
Farahksyu <ksyu.farah@gmail.com>
2024-08-04 23:07:04 +0700

    updated Hero.jsx

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -4,14 +4,14 @@ function Hero() {
   return (
     <>
       <div className="relative w-screen h-screen overflow-hidden">
-        <div className="flex">
+        <div className="flex bg-[url('/src/assets/images/ImageHero.png')] bg-no-repeat bg-cover bg-right">
           <div className="relative w-1/2">
             <img
               src="src/assets/images/Hero.png"
-              className="w-full h-screen object-cover"
+              className="w-full h-screen"
               alt="Left Image"
             />
-            <div className="absolute top-0 left-0 w-full h-full items-center justify-center text-light">
+            <div className="absolute top-0 w-full h-full items-center justify-center text-light">
               <h1 className="text-4xl p-10 font-semibold">
                 Bantu Seluruh <span className="text-secondary">Anak Yatim</span>{" "}
                 Membangun Masa Depan
@@ -35,16 +35,12 @@ function Hero() {
                 </div>
               </div>
               <div className="ml-10 mt-10">
-                <Button />
+                <Button
+                  title="Daftar Sekarang"
+                  className="bg-light text-primary"
+                />
               </div>
             </div>
-          </div>
-          <div className="relative w-1/2">
-            <img
-              src="src/assets/images/Image.png"
-              className="w-full h-screen object-cover"
-              alt="Right Image"
-            />
           </div>
         </div>
       </div>
