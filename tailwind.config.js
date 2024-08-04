cdfa6204028db698624f2ec8209209210ae14e29
Farahksyu <ksyu.farah@gmail.com>
2024-08-04 23:08:40 +0700

    displaced content tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,22 +2,22 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    colors: {
-      primary: "#518C51",
-      secondary: "#D9EADC",
-      dark: "#2A3C34",
-      light: "#FFFFFF",
-      accent: "#94EEC3",
-      text: "#333333",
-      border: "#DDDDDD",
-      disabled: "#D3D3D3",
-    },
     fontFamily: {
       raleway: ["Raleway", "sans-serif"],
     },
     extend: {
       backgroundImage: {
         hero: "url('/src/assets/images/Hero.png')",
+      },
+      colors: {
+        primary: "#518C51",
+        secondary: "#D9EADC",
+        dark: "#2A3C34",
+        light: "#FFFFFF",
+        accent: "#94EEC3",
+        text: "#333333",
+        border: "#DDDDDD",
+        disabled: "#D3D3D3",
       },
     },
   },
