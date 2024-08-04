652ce6f25134bc2ab5c5b0dbbf5f74848cb74eb9
Farahksyu <ksyu.farah@gmail.com>
2024-08-04 23:07:21 +0700

    updated Card.jsx

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,33 +1,17 @@
 import Button from "../Button/Button";
 
-function Card() {
+function Card({ title, description }) {
   return (
     <div className="flex gap-5 mt-10">
-      <div className="text-center mb-10 border bg-light rounded-lg border-[#DDDDDD]">
-        <img className="" src="src/assets/icons/Vectoricon.png" alt="" />
-        <h1>Infaq</h1>
-        <p>Lorem ipsum dolor sit amet consectetur.</p>
-        <Button />
-      </div>
-      <div className="text-center mb-10 border bg-light rounded-lg border-[#DDDDDD]">
-        <img className="" src="src/assets/icons/Vectoricon.png" alt="" />
-        <h1>Infaq</h1>
-        <p>Lorem ipsum dolor sit amet consectetur.</p>
-        <Button />
-      </div>
-      <div className="text-center mb-10 border bg-light rounded-lg border-[#DDDDDD]">
-        <img className="" src="src/assets/icons/Vectoricon.png" alt="" />
-        <h1>Infaq</h1>
-        <p>Lorem ipsum dolor sit amet consectetur.</p>
-        <Button />
-      </div>
-      <div className="text-center mb-10 border bg-light rounded-lg border-[#DDDDDD]">
-        <img className="" src="src/assets/icons/Vectoricon.png" alt="" />
-        <h1>Infaq</h1>
-        <p>Lorem ipsum dolor sit amet consectetur.</p>
+      <div className="flex flex-col max-w-[200px] items-center gap-1 p-4 text-center mb-10 border bg-light rounded-lg border-[#DDDDDD]">
+        <img className="w-14" src="src/assets/icons/Vectoricon.png" alt="" />
+        <h1>{title}</h1>
+        <p>{description}</p>
         <Button />
       </div>
     </div>
   );
 }
 export default Card;
+
+//props:property untuk menerima data
