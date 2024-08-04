44cb42279f48fc3d2849b1ae51b1e4b20a814822
Farahksyu <ksyu.farah@gmail.com>
2024-08-04 23:06:29 +0700

    added ContentData.js

---
+++ src/Data/ContentData.js
@@ -0,0 +1,20 @@
+const dataCards = [
+  {
+    title: "lorem ipsum",
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit",
+  }, //dataCard
+  {
+    title: "lorem ipsum",
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit",
+  },
+  {
+    title: "lorem ipsum",
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit",
+  },
+  {
+    title: "lorem ipsum",
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit",
+  },
+];
+
+export { dataCards };
