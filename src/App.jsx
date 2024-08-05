6afaea8befc5ca1a8909b8fd2dff44b0b9223c7b
Farahksyu <ksyu.farah@gmail.com>
2024-08-05 19:58:17 +0700

    added Aktivitas component

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Header from "./components/Header/Header";
 import Hero from "./components/Hero/Hero";
 import Section from "./components/Section/Section";
 import Layanan from "./components/Layanan/Layanan";
+import Aktivitas from "./components/Aktivitas/Aktivitas";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Hero />
       <Section />
       <Layanan />
+      <Aktivitas />
     </>
   );
 }
