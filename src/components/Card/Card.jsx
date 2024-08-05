197cf592f64134cfdc188c987cbf520319838423
Farahksyu <ksyu.farah@gmail.com>
2024-08-05 19:57:40 +0700

    updated Card.jsx

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -7,11 +7,35 @@ function Card({ title, description }) {
         <img className="w-14" src="src/assets/icons/Vectoricon.png" alt="" />
         <h1>{title}</h1>
         <p>{description}</p>
-        <Button />
+        <Button title="Donasi Sekarang" className=" text-primary" />
       </div>
     </div>
   );
 }
-export default Card;
+
+function Card2({ title, description, percentage }) {
+  return (
+    <div className="flex gap-5 mt-10">
+      <div className="flex flex-col max-w-[200px] items-center gap-1 p-4 text-center mb-10 border bg-light rounded-lg border-[#DDDDDD]">
+        <img className="w-44" src="src/assets/images/ImageHero.png" alt="" />
+        <div className="object-left">
+          <Button
+            title="tag component"
+            className="bg-secondary text-primary m-2"
+          />
+        </div>
+
+        <h1 className="font-bold">{title}</h1>
+        <div className="">
+          <p>{description}</p>
+          <p>{percentage}</p>
+        </div>
+
+        <Button title="click me" className="bg-primary text-light" />
+      </div>
+    </div>
+  );
+}
+export { Card, Card2 };
 
 //props:property untuk menerima data
