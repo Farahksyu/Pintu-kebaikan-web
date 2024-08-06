54f65704b2382d55a456925d8e7deb6d1c176963
Farahksyu <ksyu.farah@gmail.com>
2024-08-06 17:26:54 +0700

    removed Card2

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -13,29 +13,6 @@ function Card({ title, description }) {
   );
 }
 
-function Card2({ title, description, percentage }) {
-  return (
-    <div className="flex gap-5 mt-10">
-      <div className="flex flex-col max-w-[200px] items-center gap-1 p-4 text-center mb-10 border bg-light rounded-lg border-[#DDDDDD]">
-        <img className="w-44" src="src/assets/images/ImageHero.png" alt="" />
-        <div className="object-left">
-          <Button
-            title="tag component"
-            className="bg-secondary text-primary m-2"
-          />
-        </div>
-
-        <h1 className="font-bold">{title}</h1>
-        <div className="">
-          <p>{description}</p>
-          <p>{percentage}</p>
-        </div>
-
-        <Button title="click me" className="bg-primary text-light" />
-      </div>
-    </div>
-  );
-}
-export { Card, Card2 };
+export default Card;
 
 //props:property untuk menerima data
