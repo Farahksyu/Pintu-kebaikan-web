695925ae55154382435f41ff8b6f923bbbd624ad
Farahksyu <ksyu.farah@gmail.com>
2024-08-07 20:59:08 +0700

    updated Aktivitas.jsx

--- src/components/Aktivitas/Aktivitas.jsx
+++ src/components/Aktivitas/Aktivitas.jsx
@@ -4,38 +4,41 @@ import { cardActivities } from "../../Data/ContentData";
 
 function Aktivitas() {
   return (
-    <div className="relative bg-[url('/src/assets/images/Rectangle.png')] bg-no-repeat bg-cover p-5">
-      <h1
-        className="flex flex-col items-center text-3xl text-light text-bold
+    <div className="relative bg-[url('/src/assets/images/Rectangle.png')] bg-no-repeat bg-cover">
+      <div className="p-20">
+        <h1
+          className="flex flex-col items-center text-3xl text-light text-bold
       "
-      >
-        Aktivitas Penyaluran Dana
-      </h1>
-      <div className="flex flex-row justify-center gap-10 m-10">
-        <div>
-          <p className="text-light">Total dana masuk Oktober</p>
-          <h5 className="text-accent font-bold m-2">Rp 21.000.000</h5>
-        </div>
+        >
+          Aktivitas Penyaluran Dana
+        </h1>
+        <div className="flex flex-row justify-center gap-10 m-10">
+          <div>
+            <p className="text-light">Total dana masuk Oktober</p>
+            <h5 className="text-accent font-bold m-2">Rp 21.000.000</h5>
+          </div>
 
-        <div>
-          <p className="text-light">Total dana masuk tersalurkan</p>
-          <h5 className="text-accent font-bold m-2">Rp 21.000.000</h5>{" "}
+          <div>
+            <p className="text-light">Total dana masuk tersalurkan</p>
+            <h5 className="text-accent font-bold m-2">Rp 21.000.000</h5>{" "}
+          </div>
+        </div>
+        <div className="flex justify-center gap-5">
+          <Button
+            title="Sedekah Ilmu Profesi"
+            className="bg-dark text-light border border-white-500"
+          />
+          <Button
+            title="Sedekah Ilmu Profesi"
+            className="bg-dark text-light border border-white-500"
+          />
+          <Button
+            title="Sedekah Ilmu Profesi"
+            className="bg-dark text-light border border-white-500"
+          />
         </div>
       </div>
-      <div className="flex justify-center gap-5">
-        <Button
-          title="Sedekah Ilmu Profesi"
-          className="bg-dark text-light border border-white-500"
-        />
-        <Button
-          title="Sedekah Ilmu Profesi"
-          className="bg-dark text-light border border-white-500"
-        />
-        <Button
-          title="Sedekah Ilmu Profesi"
-          className="bg-dark text-light border border-white-500"
-        />
-      </div>
+
       <div className="flex flex-row justify-center gap-10">
         {cardActivities.map(
           (
