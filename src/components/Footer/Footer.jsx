86033ea3cc95116bb4a8e52475620a236e63e04c
Farahksyu <ksyu.farah@gmail.com>
2024-08-08 22:23:54 +0700

    updated Footer.jsx

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,31 +1,37 @@
 function Footer() {
   return (
     <div className="p-10">
-      <div className="flex flex-row justify-between mb-5">
+      <div className="flex flex-row justify-between mb-20">
         <div>
-          <div>
+          <div className="h-24 w-24">
             <img src="src/assets/icons/logo-mku.png" alt="" />
           </div>
-          <h5 className="font-bold">Contact</h5>
-          <p>Whatsapp: 0812492939</p>
-          <p>Instagram: @loremipsum</p>
-          <p>Email: loremipsum@gmail.com</p>
+          <h5 className="font-bold mb-4">Contact</h5>
+          <ul className="flex flex-col gap-4">
+            <li>Whatsapp: 0812492939</li>
+            <li>Instagram: @loremipsum</li>
+            <li>Email: loremipsum@gmail.com</li>
+          </ul>
         </div>
         <div>
-          <h5 className="font-bold">Layanan</h5>
-          <p>Infaq Digital</p>
-          <p>Sedekah </p>
-          <p>Zakat</p>
-          <p>Wakaf</p>
+          <h5 className="font-bold mb-4">Layanan</h5>
+          <ul className="flex flex-col gap-4">
+            <li>Infaq Digital</li>
+            <li>Sedekah </li>
+            <li>Zakat</li>
+            <li>Wakaf</li>
+          </ul>
         </div>
         <div>
-          <h5 className="font-bold">Lainnya</h5>
-          <p>Event</p>
-          <p>FAQ</p>
-          <p>Gabung Mitra</p>
+          <h5 className="font-bold mb-4">Lainnya</h5>
+          <ul className="flex flex-col gap-4">
+            <li>Event</li>
+            <li>FAQ</li>
+            <li>Gabung Mitra</li>
+          </ul>
         </div>
         <div>
-          <h5 className="font-bold">Metode Pembayaran</h5>
+          <h5 className="font-bold mb-4">Metode Pembayaran</h5>
           <div className="flex flex-row justify-center gap-5 p-2">
             <img src="src/assets/icons/spay.png" alt="" />
             <img src="src/assets/icons/gopay.png" alt="" />
@@ -38,7 +44,7 @@ function Footer() {
         </div>
       </div>
       <hr />
-      <div className="flex flex-row justify-between">
+      <div className="flex flex-row justify-between mt-5">
         <p>Copyright 2023 MRKR alright reserved</p>
         <p>Privacy Policy</p>
         <p>Terms of Services</p>
