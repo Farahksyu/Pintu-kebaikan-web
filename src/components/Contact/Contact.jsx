f56aa2242696cf72d4ba6fc2eae571149f85cbdd
Farahksyu <ksyu.farah@gmail.com>
2024-08-28 22:45:23 +0700

    updated Contact.jsx

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -29,7 +29,7 @@ function Contact() {
               className="border border-collapse w-full p-2"
             />
             <p className="mt-5">Kategori</p>
-            <div className="relative inline-block text-left">
+            <div className="relative inline-block text-left w-full">
               <div>
                 <button
                   type="button"
@@ -38,7 +38,7 @@ function Contact() {
                   aria-expanded="true"
                   aria-haspopup="true"
                 >
-                  Sub Kategori
+                  Kategori
                   <svg
                     className="-mr-1 h-5 w-5 text-gray-400"
                     viewBox="0 0 20 20"
@@ -53,58 +53,9 @@ function Contact() {
                   </svg>
                 </button>
               </div>
-
-              {/* <div
-                className="absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
-                role="menu"
-                aria-orientation="vertical"
-                aria-labelledby="menu-button"
-                tabIndex="-1"
-              >
-                <div className="py-1" role="none">
-                  <a
-                    href="#"
-                    className="block px-4 py-2 text-sm text-gray-700"
-                    role="menuitem"
-                    tabIndex="-1"
-                    id="menu-item-0"
-                  >
-                    Account settings
-                  </a>
-                  <a
-                    href="#"
-                    className="block px-4 py-2 text-sm text-gray-700"
-                    role="menuitem"
-                    tabIndex="-1"
-                    id="menu-item-1"
-                  >
-                    Support
-                  </a>
-                  <a
-                    href="#"
-                    className="block px-4 py-2 text-sm text-gray-700"
-                    role="menuitem"
-                    tabIndex="-1"
-                    id="menu-item-2"
-                  >
-                    License
-                  </a>
-                  <form method="POST" action="#" role="none">
-                    <button
-                      type="submit"
-                      className="block w-full px-4 py-2 text-left text-sm text-gray-700"
-                      role="menuitem"
-                      tabIndex="-1"
-                      id="menu-item-3"
-                    >
-                      Sign out
-                    </button>
-                  </form>
-                </div>
-              </div> */}
             </div>
             <p className="mt-5">Sub Kategori</p>
-            <div className="relative inline-block text-left">
+            <div className="relative inline-block text-left w-full">
               <div>
                 <button
                   type="button"
@@ -115,7 +66,7 @@ function Contact() {
                 >
                   Sub Kategori
                   <svg
-                    className="-mr-1 h-5 w-5 text-gray-400"
+                    className="-mr-1 h-5 w-10 text-gray-400"
                     viewBox="0 0 20 20"
                     fill="currentColor"
                     aria-hidden="true"
